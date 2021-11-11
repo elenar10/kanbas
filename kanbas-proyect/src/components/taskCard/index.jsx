282b34c7a6e042fcee4fd4c050a9f77e7dbea619
SRVONDINA\Elena <elrome.tronel@gmail.com>
2021-11-11 16:51:40 +0100

    cambio de props

--- kanbas-proyect/src/components/taskCard/index.jsx
+++ kanbas-proyect/src/components/taskCard/index.jsx
@@ -2,18 +2,18 @@ import './style.css'
 import React from 'react';
 
 
-function TaskCard(props) {
+function TaskCard({tarea, id, fecha, estadoIcon}) {
     function deleteTaskCard(e){
         console.log(e)
-    //   localStorage.removeItem(`task${props.results.id}`)
+      localStorage.removeItem(`task${id}`)
     }
    
     return (
         <div className='cardTasK_contenedor'>
-            {/* <p><span className="iconChecked"> check_circle</span></p>
+            <p><span className="iconChecked"> check_circle</span></p>
             <p><span className="iconInprogress">pending</span></p> */}
-            {/* <h3 className="task__name">{props.results.tarea}</h3>
-            <p className="task__date">{props.results.id} {props.results.fecha}</p> */}
+            <h3 className="task__name">{tarea}</h3>
+            <p className="task__date">{id} {fecha}</p>
             <p><span className="iconDelete" onClick={deleteTaskCard}>🗑️</span> </p>
         
         </div>
