5763bd9c5f1cd055791e77b28499cffe26bab88e
MartinYela <martinyela@hotmail.com>
2021-11-13 21:14:52 +0100

    card agregadas (sin css) y contador de task list funcionando

---
+++ kanbas-proyect/src/components/task-card/task-card.jsx
@@ -0,0 +1,26 @@
+import './style.css'
+
+function TaskCard(props) {
+    
+    // recuperamos del localStorage el objeto por su id
+//    let resultsObjet= localStorage.getItem(`task${props.results.id}`);
+//    let estadoIcons= JSON.parse(resultsObjet)
+    function handleClickDelete(){
+        localStorage.removeItem(`task${props.results.id}`)
+        props.onUpdateTaskList();
+       
+    }
+    return (
+        <div className='cardTasK_contenedor'>
+            <div className='cardTask_tilte-Icons'>
+                {/* {estadoIcons?.estadoIcono === 'Pending'? 
+                <p><span className="iconInprogress">pending</span></p>
+                :<p><span className="iconChecked"> check_circle</span></p>} */}
+                <h3 className="task__name">{props.results.tarea}</h3>
+                <p><span className="iconDelete" onClick={handleClickDelete}>:papelera:</span></p>
+            </div>
+            <p className="task__date">#{props.results.id} {props.results.fecha}h</p>
+        </div>
+    )
+}
+export default TaskCard;
--- kanbas-proyect/src/components/task-list/index.jsx
+++ kanbas-proyect/src/components/task-list/index.jsx
@@ -1,56 +1,58 @@
 import React, { Fragment, useEffect, useState } from "react";
 import './style.css'
 import Form from '../form'
-import Prueba from "./prueba";
+import TaskCard from "../task-card/task-card";
+
 
 function TaskList(props) {
-    const [addTask, setAddTask] = useState(false);
-    const [task, setTask] = useState([]);
-    const [draw, setDraw] = useState(true);
+    const [addTask, setAddTask] = useState(false); //indica si el form se pinta o no
+    const [task, setTask] = useState([]);     //Es el array con las tareas de la localStorage
+    const [draw, setDraw] = useState(true);     // para renderizar de nuevo cuando los hijos tengan cambios
 
-//inicializo el contador al valor de la local Storage, si no hay valor todavía, lo inicializo en 1
-    let counter = localStorage.getItem('counter') ?? 1;  
 
-// función que cambia una variable de estado, para que al hacer click en el'+' para abrir el formulario, se actualice, haga render otra vez, y abra el formulario
+    //inicializo el contador al valor de la local Storage, si no hay valor todavía, lo inicializo en 1
+    let counter = localStorage.getItem('counter') ?? 1;
+
+    // función que cambia una variable de estado, para que al hacer click en el'+' para abrir el formulario, se actualice, haga render otra vez, y abra el formulario
     function openForm() {
-        addTask === true ? setAddTask(false):setAddTask(true)
-        }
+        addTask === true ? setAddTask(false) : setAddTask(true)
+    }
 
 
-// funcion que le paso al formulario como props, para que cuando ingreso una nueva tarea, el form llame a está función, y se modifique la varible de estado para volver a pintar, pero ahora con la nueva tarea guardada
+    // funcion que le paso al formulario como props, para que cuando ingreso una nueva tarea, el form llame a está función, y se modifique la varible de estado para volver a pintar, pero ahora con la nueva tarea guardada
     const drawTaskList = () => {
         draw ? setDraw(false) : setDraw(true);
         addTask ? setAddTask(false) : setAddTask(true)
     }
 
-// está función sería como el componentDidMount que usabamos en la clase, para que al abrir el programa, recoja las tareas de la localStorage y pinte las cards
-    useEffect (() => {
+    // está función sería como el componentDidMount que usabamos en la clase, para que al abrir el programa, recoja las tareas de la localStorage y pinte las cards
+    useEffect(() => {
         let arr = [];
-        for (let i =1; i<counter;i++){
-                if (JSON.parse(localStorage.getItem(`task${i}`)!== null)){
-                    arr.push(JSON.parse(localStorage.getItem(`task${i}`)))
-                }
+        for (let i = 1; i < counter; i++) {
+            if (JSON.parse(localStorage.getItem(`task${i}`) !== null)) {
+                arr.push(JSON.parse(localStorage.getItem(`task${i}`)))
+            }
         }
-       setTask(arr)
-    }, [counter])
+        setTask(arr)
+
+    }, [counter, draw])
 
     return (
         <Fragment>
             <div className="taskList__container">
                 <div className="taskList__header">
                     <div className="title-counter__wrapper">
-                        <div className="counter">12</div>
+                        <div className="counter">{task.filter(e => props.title === e.estado).length}</div>
                         <h3 className="task__title">{props.title}</h3>
                     </div>
                     <button className="button__add" onClick={openForm}>+</button>
                     {props.remove}
                 </div>
                 {addTask ? <Form onUpdateTaskList={drawTaskList} titleTask={props.title}></Form> : ' '}
-                {task.map((e) => props.title === e.estado ?<Prueba key={e.id} results={e}></Prueba>:'')}
+                {task.map((e) => props.title === e.estado ? <TaskCard key={e.id} results={e} onUpdateTaskList={() => draw ? setDraw(false) : setDraw(true)}></TaskCard> : '')}
             </div>
         </Fragment>
     )
 }
 
 export default TaskList;
-
