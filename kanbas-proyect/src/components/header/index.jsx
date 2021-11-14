81ed4af29bbe89e2f180b1ee7dcc45b5a1f030cd
adaldin <aledaldin@gmail.com>
2021-11-14 10:26:29 +0100

    Subida Icon a header (png/jsx/css)

--- kanbas-proyect/src/components/header/index.jsx
+++ kanbas-proyect/src/components/header/index.jsx
@@ -1,14 +1,18 @@
 import './style.css'
 import logo from './logo.png';
+import icon from './icon-version.png';
 
 function Header() {
     return (
         <header className="App-header">
             <div className="logo-version__wrapper">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>Version 1.0.0</p>
+                <div className="version-icon__wrapper">
+                    <img src={icon} className="icon-version" alt="verion-icon" />
+                    <p>Version 1.0.0</p>
+                </div>
             </div>
-        </header>
+        </header >
 
     )
 }
