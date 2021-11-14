a63de63e6792fa3716160a2c8d41e3c3cfec5545
MartinYela <martinyela@hotmail.com>
2021-11-14 14:54:15 +0100

    a

--- kanbas-proyect/src/components/task-card/task-card.jsx
+++ kanbas-proyect/src/components/task-card/task-card.jsx
@@ -3,8 +3,8 @@ import './style.css'
 function TaskCard(props) {
     
     // recuperamos del localStorage el objeto por su id
-//    let resultsObjet= localStorage.getItem(`task${props.results.id}`);
-//    let estadoIcons= JSON.parse(resultsObjet)
+   let resultsObjet= localStorage.getItem(`task${props.results.id}`);
+   let estadoIcons= JSON.parse(resultsObjet)
     function handleClickDelete(){
         localStorage.removeItem(`task${props.results.id}`)
         props.onUpdateTaskList();
@@ -13,9 +13,9 @@ function TaskCard(props) {
     return (
         <div className='cardTasK_contenedor'>
             <div className='cardTask_tilte-Icons'>
-                {/* {estadoIcons?.estadoIcono === 'Pending'? 
+                {estadoIcons?.estadoIcono === 'Pending'? 
                 <p><span className="iconInprogress">pending</span></p>
-                :<p><span className="iconChecked"> check_circle</span></p>} */}
+                :<p><span className="iconChecked"> check_circle</span></p>}
                 <h3 className="task__name">{props.results.tarea}</h3>
                 <p><span className="iconDelete" onClick={handleClickDelete}>:papelera:</span></p>
             </div>
