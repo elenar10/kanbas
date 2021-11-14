d55ebb49eede380499a27a4944dcccef312f13d7
adaldin <aledaldin@gmail.com>
2021-11-14 08:40:32 +0100

    Actualización del css de taskList por el que contiene el wrapper del buttton + y Clear All

--- kanbas-proyect/src/components/form/index.jsx
+++ kanbas-proyect/src/components/form/index.jsx
@@ -7,7 +7,7 @@ function Form(props) {
     const [title, setTitle] = useState('');
     let counter = localStorage.getItem('counter') ?? 1;
 
-// comprueba si el input tiene algo escrito, si es así, la variable de estado 'btnEnabled' se pone true, si no tiene nada se pone 'false'. Esto es para que en función de ella, el botón quede con opacidad o no.
+    // comprueba si el input tiene algo escrito, si es así, la variable de estado 'btnEnabled' se pone true, si no tiene nada se pone 'false'. Esto es para que en función de ella, el botón quede con opacidad o no.
     const comprobationInput = (e) => {
         if (e.target.value === '') {
             setBtnEnabled(false);
@@ -16,17 +16,20 @@ function Form(props) {
         }
     }
 
-// está función es la del evento onClick del form. Se encarga de recoger fecha y hora actual, armar un objeto con los valores de la tarea y almacenarlos en la localStorage. También actualiza el contador para poder guardar una siguiente tarea
+    // está función es la del evento onClick del form. 
+    // Se encarga de recoger fecha y hora actual, armar un objeto con 
+    // los valores de la tarea y almacenarlos en la localStorage. 
+    // También actualiza el contador para poder guardar una siguiente tarea
     const sendTarea = (e) => {
         e.preventDefault();
         const day = new Date();
-        const fecha = `${day.getDate()}/${day.getMonth() + 1 }/${day.getFullYear()} ${day.getHours()}:${day.getMinutes()}:${day.getSeconds()}`;
+        const fecha = `${day.getDate()}/${day.getMonth() + 1}/${day.getFullYear()} ${day.getHours()}:${day.getMinutes()}:${day.getSeconds()}`;
         const objectTask = {
-            id : counter,
+            id: counter,
             tarea: e.target.task.value,
             fecha: fecha,
             estado: props.titleTask,
-            estadoIcono : props.titleTask === 'Done' ? 'Done': 'Pending'
+            estadoIcono: props.titleTask === 'Done' ? 'Done' : 'Pending'
         }
         localStorage.setItem(`task${counter}`, JSON.stringify(objectTask));
         setTitle(e.target.task.value);
@@ -46,7 +49,7 @@ function Form(props) {
                         <button className='btn btn_cancelar' type='reset' onClick={() => setBtnEnabled(false)}>Cancel</button>
                     </div>
                 </form>
-                :''}
+                : ''}
         </Fragment>
     )
 }
--- kanbas-proyect/src/components/task-list/index.jsx
+++ kanbas-proyect/src/components/task-list/index.jsx
@@ -8,30 +8,30 @@ function TaskList(props) {
     const [task, setTask] = useState([]);
     const [draw, setDraw] = useState(true);
 
-//inicializo el contador al valor de la local Storage, si no hay valor todavía, lo inicializo en 1
-    let counter = localStorage.getItem('counter') ?? 1;  
+    //inicializo el contador al valor de la local Storage, si no hay valor todavía, lo inicializo en 1
+    let counter = localStorage.getItem('counter') ?? 1;
 
-// función que cambia una variable de estado, para que al hacer click en el'+' para abrir el formulario, se actualice, haga render otra vez, y abra el formulario
+    // función que cambia una variable de estado, para que al hacer click en el'+' para abrir el formulario, se actualice, haga render otra vez, y abra el formulario
     function openForm() {
-        addTask === true ? setAddTask(false):setAddTask(true)
-        }
+        addTask === true ? setAddTask(false) : setAddTask(true)
+    }
 
 
-// funcion que le paso al formulario como props, para que cuando ingreso una nueva tarea, el form llame a está función, y se modifique la varible de estado para volver a pintar, pero ahora con la nueva tarea guardada
+    // funcion que le paso al formulario como props, para que cuando ingreso una nueva tarea, el form llame a está función, y se modifique la varible de estado para volver a pintar, pero ahora con la nueva tarea guardada
     const drawTaskList = () => {
         draw ? setDraw(false) : setDraw(true);
         addTask ? setAddTask(false) : setAddTask(true)
     }
 
-// está función sería como el componentDidMount que usabamos en la clase, para que al abrir el programa, recoja las tareas de la localStorage y pinte las cards
-    useEffect (() => {
+    // está función sería como el componentDidMount que usabamos en la clase, para que al abrir el programa, recoja las tareas de la localStorage y pinte las cards
+    useEffect(() => {
         let arr = [];
-        for (let i =1; i<counter;i++){
-                if (JSON.parse(localStorage.getItem(`task${i}`)!== null)){
-                    arr.push(JSON.parse(localStorage.getItem(`task${i}`)))
-                }
+        for (let i = 1; i < counter; i++) {
+            if (JSON.parse(localStorage.getItem(`task${i}`) !== null)) {
+                arr.push(JSON.parse(localStorage.getItem(`task${i}`)))
+            }
         }
-       setTask(arr)
+        setTask(arr)
     }, [counter])
 
     return (
@@ -42,11 +42,13 @@ function TaskList(props) {
                         <div className="counter">12</div>
                         <h3 className="task__title">{props.title}</h3>
                     </div>
-                    <button className="button__add" onClick={openForm}>+</button>
-                    {props.remove}
+                    <div className="remove__container">
+                        <button className="button__add" onClick={openForm}>+</button>
+                        <button className="btn_clearAll">{props.remove}</button>
+                    </div>
                 </div>
                 {addTask ? <Form onUpdateTaskList={drawTaskList} titleTask={props.title}></Form> : ' '}
-                {task.map((e) => props.title === e.estado ?<Prueba key={e.id} results={e}></Prueba>:'')}
+                {task.map((e) => props.title === e.estado ? <Prueba key={e.id} results={e}></Prueba> : '')}
             </div>
         </Fragment>
     )
