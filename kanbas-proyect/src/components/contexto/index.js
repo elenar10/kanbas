4510fadf1445bf05fd3b4799db1008cf964aa2e7
MartinYela <martinyela@hotmail.com>
2021-11-17 09:22:46 +0100

    update arreglado

--- kanbas-proyect/src/components/contexto/index.js
+++ kanbas-proyect/src/components/contexto/index.js
@@ -1,3 +1,3 @@
 import { createContext } from "react";
 
-export const DateContext = createContext(localStorage.getItem('lastUpdate') ?? 'no ha realizado tarea todavía');
+export const DateContext = createContext('onUpdate');
