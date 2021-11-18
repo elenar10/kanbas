255b0fa3a5f0be93b78a4e9cf0b17602387f6c28
adaldin <aledaldin@gmail.com>
2021-11-18 17:31:23 +0100

    Actualización de version from package.json+ícono versión

--- kanbas-proyect/src/components/header/index.jsx
+++ kanbas-proyect/src/components/header/index.jsx
@@ -1,6 +1,8 @@
 import './style.css'
 import logo from './logo.png';
 import icon from './icon-version.png';
+import { version } from '../../../package.json'
+
 
 function Header() {
     return (
@@ -9,7 +11,7 @@ function Header() {
                 <img src={logo} className="App-logo" alt="logo" />
                 <div className="version-icon__wrapper">
                     <img src={icon} className="icon-version" alt="verion-icon" />
-                    <p>Version 1.0.0</p>
+                    <p>Version {version}</p>
                 </div>
             </div>
         </header >
